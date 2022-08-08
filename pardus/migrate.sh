ff60018a39b42ecd3d36efb5afb6fa80ae9a9419
sulincix <55052890+sulincix@users.noreply.github.com>
2022-08-08 12:53:19 +0000

    Update migrate.sh

--- pardus/migrate.sh
+++ pardus/migrate.sh
@@ -4,8 +4,10 @@ deb http://deb.devuan.org/merged stable-updates main
 deb http://deb.devuan.org/merged stable-security main
 #deb http://deb.devuan.org/merged stable-backports main
 EOF
+rm -f /var/lib/dpkg/info/systemd.prerm
 apt-get update --allow-insecure-repositories
 apt-get install devuan-keyring --allow-unauthenticated -y
 apt-get update
+apt install elogind eudev sysvinit-core sysv-rc -y --allow-remove-essential -y
 apt-get full-upgrade -y
 apt-get autoremove --purge -y
