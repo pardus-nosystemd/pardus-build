b8b0afab8569658933beae9a1183db981ee5000b
sulincix <55052890+sulincix@users.noreply.github.com>
2022-10-02 14:56:58 +0000

    Update package-updates.sh

--- pardus-gnome/package-updates.sh
+++ pardus-gnome/package-updates.sh
@@ -2,8 +2,8 @@ apt update
 apt install wget bzip2 zenity gvfs-fuse -y
 apt full-upgrade -y
 apt purge libreoffice-common -y
-apt purge firefox-esr chromium -y
+apt purge firefox-esr gnome-core -y
 apt install librewolf -y
 apt install mpv -y
-apt purge audacious gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver gimp xsane evolution synaptic drawing catfish -y
+apt purge gdebi packagekit xterm yelp vlc gimp xsane evolution synaptic drawing baobab brasero -y
 apt autoremove --purge -y
