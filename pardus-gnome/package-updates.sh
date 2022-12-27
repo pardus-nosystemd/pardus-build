03752fe6dadc9a00a090c5ac50ced4aa45225d55
sulincix <parduscix@yandex.ru>
2022-12-27 21:08:38 +0000

    remove gnome bloat

--- pardus-gnome/package-updates.sh
+++ pardus-gnome/package-updates.sh
@@ -4,5 +4,5 @@ apt full-upgrade -y -o Dpkg::Options::="--force-confnew"
 apt purge libreoffice-common -y
 apt purge firefox-esr -y
 apt install librewolf -y -o Dpkg::Options::="--force-confnew"
-apt purge audacious gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver gimp xsane evolution synaptic drawing catfish -y
+apt purge yelp gdebi packagekit xterm chromium vlc gimp xsane evolution synaptic drawing gnome-logs baobab gnome-contacts gnome-characters -y
 apt autoremove --purge -y
