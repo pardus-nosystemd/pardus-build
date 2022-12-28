0cec50bc89f5252b4b52f6834001151db51a885c
sulincix <parduscix@yandex.ru>
2022-12-28 07:01:58 +0000

    Update package-updates.sh

--- pardus-gnome/package-updates.sh
+++ pardus-gnome/package-updates.sh
@@ -1,8 +1,8 @@
 apt update
-apt install wget bzip2 zenity gvfs-fuse -y -o Dpkg::Options::="--force-confnew"
-apt full-upgrade -y -o Dpkg::Options::="--force-confnew"
+apt install wget bzip2 zenity gvfs-fuse -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
+apt full-upgrade -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
 apt purge libreoffice-common -y
 apt purge firefox-esr -y
-apt install librewolf -y -o Dpkg::Options::="--force-confnew"
+apt install librewolf -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
 apt purge yelp gdebi packagekit xterm chromium vlc gimp xsane evolution synaptic drawing gnome-logs baobab gnome-contacts gnome-characters -y
 apt autoremove --purge -y
