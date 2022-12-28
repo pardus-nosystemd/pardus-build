3222772294a0594154401621b6530b768be50fa2
sulincix <parduscix@yandex.ru>
2022-12-28 07:02:20 +0000

    Update package-updates.sh

--- pardus-kde/package-updates.sh
+++ pardus-kde/package-updates.sh
@@ -1,8 +1,8 @@
 apt update
-apt install wget bzip2 zenity gvfs-fuse -y -o Dpkg::Options::="--force-confnew"
-apt full-upgrade -y -o Dpkg::Options::="--force-confnew"
+apt install wget bzip2 zenity gvfs-fuse -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
+apt full-upgrade -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
 apt purge libreoffice-common -y
 apt purge firefox-esr -y
-apt install librewolf -y -o Dpkg::Options::="--force-confnew"
-apt purge audacious gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver gimp xsane evolution synaptic drawing catfish -y
+apt install librewolf -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
+apt purge audacious gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver gimp xsane evolution synaptic drawing catfish -y 
 apt autoremove --purge -y
