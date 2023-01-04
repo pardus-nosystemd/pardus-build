6c6790160060cc49a34792cb87032760853a9d3c
sulincix <parduscix@yandex.ru>
2023-01-04 11:49:01 +0000

    Update migrate.sh

--- common/migrate.sh
+++ common/migrate.sh
@@ -7,7 +7,6 @@ apt-get install devuan-keyring wget --allow-unauthenticated -y -o Dpkg::Options:
 apt-get update
 cd /tmp
 wget https://github.com/my-garbage-stuff/systemd-dummy/releases/download/current/systemd_9999-noupdate_all.deb
-wget https://github.com/my-garbage-stuff/systemd-dummy/releases/download/current/systemd-sysv_9999-noupdate_all.deb
 dpkg -i /tmp/*.deb
 ln -s true /bin/systemctl # fake systemctl
 apt-get install elogind eudev sysvinit-core sysv-rc ntp wget -y --allow-remove-essential -y -o Dpkg::Options::="--force-confnew"
