286fb72eedede0e48ccfcc170850c17bfa9be030
aliriza <aliriza.keskin@pardus.org.tr>
2023-01-04 09:05:19 +0000

    devuan fix

--- common/migrate.sh
+++ common/migrate.sh
@@ -6,6 +6,8 @@ apt-mark hold desktop-base # Do not replace theme
 apt-get update --allow-insecure-repositories
 apt-get install devuan-keyring --allow-unauthenticated -y -o Dpkg::Options::="--force-confnew"
 apt-get update
-apt install elogind eudev sysvinit-core sysv-rc ntp -y --allow-remove-essential -y -o Dpkg::Options::="--force-confnew"
+apt-get purge systemd
+ln -s true /bin/systemctl # fake systemctl
+apt-install elogind eudev sysvinit-core sysv-rc ntp -y --allow-remove-essential -y -o Dpkg::Options::="--force-confnew"
 apt-get full-upgrade -y -o Dpkg::Options::="--force-confnew"
 apt-get autoremove --purge -y
