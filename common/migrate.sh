f25999ff565495eca2b75160f87cccd1176618ff
aliriza <aliriza.keskin@pardus.org.tr>
2023-01-05 06:30:51 +0000

    fixes

--- common/migrate.sh
+++ common/migrate.sh
@@ -9,7 +9,7 @@ cd /tmp
 wget https://github.com/my-garbage-stuff/systemd-dummy/releases/download/current/systemd_9999-noupdate_all.deb
 wget https://github.com/pardus-nosystemd/desktop-base/releases/download/current/desktop-base_9999-noupdate_all.deb
 wget https://github.com/pardus-nosystemd/base-files/releases/download/current/base-files_9999-noupdate_amd64.deb
-dpkg -i /tmp/*.deb
+apt install ./*.deb -yq --allow-downgrades
 ln -s true /bin/systemctl # fake systemctl
 apt-get install elogind eudev sysvinit-core sysv-rc ntp wget -y --allow-remove-essential -y -o Dpkg::Options::="--force-confnew"
 apt-get full-upgrade -y -o Dpkg::Options::="--force-confnew"
