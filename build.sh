d0fdc722c789c98cad26117807db257be5219fcb
sulincix <parduscix@yandex.ru>
2023-01-28 14:13:05 +0000

    new editions

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 \
 make && make install
 ln -s sid /usr/share/debootstrap/scripts/yirmibir || true
 cd ../
-for desktop in xfce gnome kde cinnamon mate ; do
+for desktop in xfce gnome kde cinnamon mate budgie ; do
     mkteaiso --profile=./pardus-$desktop --output=/output/ --debug 2>&1 &
 done
 wait
