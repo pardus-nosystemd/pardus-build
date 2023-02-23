782a4bbae494f8eb180c51837aae50703d3ec94f
sulincix <parduscix@yandex.ru>
2023-02-23 17:13:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -ex
+apt update
+apt install xorriso grub-pc-bin grub-efi mtools make python3 \
+    dosfstools e2fsprogs squashfs-tools python3-yaml \
+    gcc wget curl unzip xz-utils zstd debootstrap git -y --no-install-recommends
 git clone https://gitlab.com/tearch-linux/applications-and-tools/teaiso
 cd teaiso
-sudo apt update
-sudo apt install xorriso grub-pc-bin grub-efi mtools make python3 \
-    dosfstools e2fsprogs squashfs-tools python3-yaml \
-    gcc wget curl unzip xz-utils zstd debootstrap -y --no-install-recommends
 make && make install
 ln -s sid /usr/share/debootstrap/scripts/yirmibir || true
 cd ../
