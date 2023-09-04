0e26c1aa287e4a78e9e504b0017528f3d35ef8f5
sulincix <parduscix@yandex.ru>
2023-09-04 07:33:54 +0000

    Update pardus-keyring.sh

--- common/pardus-keyring.sh
+++ common/pardus-keyring.sh
@@ -1,6 +1,6 @@
 cat > /etc/apt/sources.list.d/pardus.list << EOF
 deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free
-deb http://depo.pardus.org.tr/guvenlik yirmiuc main contrib non-free
+#deb http://depo.pardus.org.tr/guvenlik yirmiuc main contrib non-free
 #deb http://depo.pardus.org.tr/backports yirmiuc main contrib non-free
 EOF
 apt-get update --allow-insecure-repositories
