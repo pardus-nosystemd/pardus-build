3d1c8bd407b2201619e5d35edf18e10924ea38fb
sulincix <parduscix@yandex.ru>
2023-09-04 07:52:35 +0000

    Update package-updates.sh

--- common/package-updates.sh
+++ common/package-updates.sh
@@ -3,6 +3,5 @@ apt install wget bzip2 zenity gvfs-fuse -y -o Dpkg::Options::="--force-confnew" 
 apt full-upgrade -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
 apt purge libreoffice-common -y
 apt purge firefox-esr -y
-apt install librewolf -y -o Dpkg::Options::="--force-confnew" --allow-unauthenticated
 apt purge yelp gdebi packagekit xterm chromium vlc gimp xsane evolution synaptic drawing gnome-logs baobab gnome-contacts gnome-characters -y
 apt autoremove --purge -y
