208b0029a4cc34421bcbd57fbee7cb3e3f90abf7
sulincix <parduscix@yandex.ru>
2024-03-02 20:46:49 +0000

    Update migrate.sh

--- common/migrate.sh
+++ common/migrate.sh
@@ -1,5 +1,6 @@
 cat > /etc/apt/sources.list << EOF
 deb http://deb.devuan.org/merged testing main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian testing main contrib non-free non-free-firmware
 EOF
 rm -f /var/lib/dpkg/info/systemd.prerm
 apt-get update --allow-insecure-repositories
