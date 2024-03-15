e236eddd31b506cbfecae470b923563a9dd3bd91
sulincix <parduscix@yandex.ru>
2024-03-15 07:10:16 +0000

    Update pipewire.sh

--- common/pipewire.sh
+++ common/pipewire.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /tmp
-apt install wget pipewire pipewire-pulse pipewre-alsa pipewire-audio pipewire-v4l2 -y
+apt install wget pipewire pipewire-pulse pipewire-alsa pipewire-audio pipewire-v4l2 -y
 wget https://github.com/aleyna-tilki/pipewire-launcher/releases/download/current/pipewire-launcher_1.0.0_all.deb
 apt install ./*.deb -yq --allow-downgrades
 
