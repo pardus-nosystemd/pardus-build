4b031996b759c8739a269214469a172e3870ece2
sulincix <parduscix@yandex.ru>
2025-08-06 12:53:33 +0300

    fixes

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
-: apt update
-: apt install xorriso grub-pc-bin grub-efi mtools make python3 libc6-dev \
-    dosfstools e2fsprogs squashfs-tools python3-yaml ca-certificates \
-    gcc wget curl unzip xz-utils zstd debootstrap git -y --no-install-recommends
+if command -v apt ; then
+    apt update
+    apt install xorriso grub-pc-bin grub-efi mtools make python3 libc6-dev \
+        dosfstools e2fsprogs squashfs-tools python3-yaml ca-certificates \
+        gcc wget curl unzip xz-utils zstd debootstrap git -y --no-install-recommends
+fi
 ln -s sid /usr/share/debootstrap/scripts/yirmiuc-deb || true
 curdir=$PWD
 for desktop in xfce gnome kde cinnamon; do
