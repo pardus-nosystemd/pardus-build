981dfc2186b1d3c82a65febf1ea27ff66148304b
sulincix <parduscix@yandex.ru>
2025-08-09 13:25:15 +0000

    Update 00pardus.sh

--- common/00pardus.sh
+++ common/00pardus.sh
@@ -2,12 +2,6 @@ cat > /etc/apt/sources.list.d/pardus.list << EOF
 deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free
 deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free
 deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free
-deb http://depo.pardus.org.tr/backports yirmiuc-backports main contrib non-free non-free-firmware
-EOF
-cat > /etc/apt/preferences.d/yirmiuc-backports_default << EOF
-Package: *
-Pin: release n=yirmiuc-backports
-Pin-Priority: 500
 EOF
 apt-get update --allow-insecure-repositories
 apt-get install pardus-archive-keyring --allow-unauthenticated -y -o Dpkg::Options::="--force-confnew"
