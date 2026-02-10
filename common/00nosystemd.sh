e46bfa2a14720ce59197e5c6666034845230624f
sulincix <parduscix@yandex.ru>
2026-02-10 14:22:02 +0000

    Update 00nosystemd.sh

--- common/00nosystemd.sh
+++ common/00nosystemd.sh
@@ -1,6 +1,8 @@
 
 cat > /etc/apt/sources.list << EOF
 deb http://deb.devuan.org/merged stable main contrib non-free non-free-firmware
+deb http://deb.devuan.org/merged stable-security main contrib non-free non-free-firmware
+deb http://deb.devuan.org/merged stable-updates main contrib non-free non-free-firmware
 EOF
 
 rm -f /var/lib/dpkg/info/systemd.p*
@@ -8,7 +10,9 @@ apt-get update --allow-insecure-repositories
 apt-get install devuan-keyring -y --allow-unauthenticated
 apt-get update
 apt install elogind sysvinit-core openrc systemd- systemd-sysv- -y --allow-remove-essential
-apt-mark hold systemd libsystemd0 libsystemd-shared
+apt-mark hold systemd || true
+apt-mark hold libsystemd0 || true
+apt-mark hold libsystemd-shared || true
 apt-get full-upgrade -y -o Dpkg::Options::="--force-confnew"
 apt-get autoremove --purge -y
 ln -s true /bin/systemctl || true
