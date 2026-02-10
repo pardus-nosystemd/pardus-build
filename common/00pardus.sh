9db89774d5d5e5834d5517206967a55cc8653660
sulincix <parduscix@yandex.ru>
2026-02-10 14:05:44 +0000

    Update 00pardus.sh

--- common/00pardus.sh
+++ common/00pardus.sh
@@ -1,7 +1,7 @@
 cat > /etc/apt/sources.list.d/pardus.list << EOF
-deb http://depo.pardus.org.tr/pardus yirmiuc main contrib non-free
-deb http://depo.pardus.org.tr/pardus yirmiuc-deb main contrib non-free
-deb http://depo.pardus.org.tr/guvenlik yirmiuc-deb main contrib non-free
+deb http://depo.pardus.org.tr/pardus yirmibes main contrib non-free
+deb http://depo.pardus.org.tr/pardus yirmibes-deb main contrib non-free
+deb http://depo.pardus.org.tr/guvenlik yirmibes-deb main contrib non-free
 EOF
 apt-get update --allow-insecure-repositories
 apt-get install pardus-archive-keyring --allow-unauthenticated -y -o Dpkg::Options::="--force-confnew"
