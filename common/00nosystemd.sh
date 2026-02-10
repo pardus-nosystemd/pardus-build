c928f51491be71cbabee151756f493bf79e4197a
sulincix <parduscix@yandex.ru>
2026-02-10 14:12:20 +0000

    Update and rename 01nosystemd.sh to 00nosystemd.sh

--- common/00nosystemd.sh
+++ common/00nosystemd.sh
@@ -7,9 +7,8 @@ rm -f /var/lib/dpkg/info/systemd.p*
 apt-get update --allow-insecure-repositories
 apt-get install devuan-keyring -y --allow-unauthenticated
 apt-get update
-apt-get purge systemd-timesyncd systemd -yq || true
-apt-get install devuan-keyring elogind libpam-elogind eudev sysvinit-core sysv-rc ntp wget -y --allow-remove-essential -y -o Dpkg::Options::="--force-confnew"
+apt install elogind sysvinit-core openrc systemd- systemd-sysv- -y --allow-remove-essential
+apt-mark hold systemd libsystemd0 libsystemd-shared
 apt-get full-upgrade -y -o Dpkg::Options::="--force-confnew"
 apt-get autoremove --purge -y
-apt-mark hold systemd libsystemd0
 ln -s true /bin/systemctl || true
